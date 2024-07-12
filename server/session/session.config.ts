714d965cabd16b8462321cd4942bfd098575362d
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-07-12 17:12:53 +0000

    added session files

---
+++ server/session/session.config.ts
@@ -0,0 +1,26 @@
+import { SessionOptions } from "iron-session";
+
+export interface SessionData {
+  userId?: string;
+  email?: string;
+  firstName?: string;
+  lastName?: string;
+  image?: string;
+  isPremium?: boolean;
+  isOnboarded?: boolean;
+  isVerified?: boolean;
+  isLoggedIn: boolean;
+}
+
+export const defaultSession: SessionData = {
+  isLoggedIn: false,
+};
+
+export const sessionOptions: SessionOptions = {
+  password: process.env.SESSION_SECRET!,
+  cookieName: "mileston-session",
+  cookieOptions: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  },
+};
