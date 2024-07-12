39e8b869e0693e9158c3dae0ea76a919d94e36fe
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-07-12 17:21:40 +0000

    auth: added signin pages

---
+++ app/auth/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Sumffy.ai | Auth",
+  description: "Personalized AI Activity Suggestion App. Brainstorm | Ideate | Create!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <p>This is the sign in page</p>
+    )
+}
