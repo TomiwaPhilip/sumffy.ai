032a01ba15a6ea46eb5bf6e6478c325c003939af
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-07-12 17:16:32 +0000

    updated: file structure

--- server/session/session.config.ts
+++ server/session/session.config.ts
@@ -18,7 +18,7 @@ export const defaultSession: SessionData = {
 
 export const sessionOptions: SessionOptions = {
   password: process.env.SESSION_SECRET!,
-  cookieName: "mileston-session",
+  cookieName: "sumffyai-session",
   cookieOptions: {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
