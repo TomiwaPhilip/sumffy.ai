2fb0dec5f3fb12da2dcd6967931ef9a9dc457c0d
TomiwaPhilip2 <166803807+TomiwaPhilip2@users.noreply.github.com>
2024-07-18 20:04:13 +0000

    updated

--- app/api/ip-address/route.ts
+++ app/api/ip-address/route.ts
@@ -5,5 +5,5 @@ export async function GET(req: NextRequest, res: NextResponse) {
     console.log("I got to API")
     const ip = ipAddress(req);
     console.log("user Ip:", ip);
-    Response.json({ ip });
+    return Response.json({ ip });
 }
