2b7dfa383b968ad5ec1c6e6303fcdaee1835766c
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-07-21 12:56:34 +0000

    updated

--- app/api/ip-address/route.ts
+++ app/api/ip-address/route.ts
@@ -3,7 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
   console.log("I got to API");
-  const {latitude, longitude, ...data} = geolocation(req);
+  const { latitude, longitude, ...data } = geolocation(req);
   console.log("user geolaocation:", data);
+  console.log("latitude:", latitude);
+  console.log("longitude:", longitude);
   return Response.json({ latitude, longitude });
 }
