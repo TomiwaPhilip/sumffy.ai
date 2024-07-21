76437ef6c9df6aebe5b626f32ed7ce58d43b2f34
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-07-21 11:06:34 +0000

    updated

--- app/api/ip-address/route.ts
+++ app/api/ip-address/route.ts
@@ -1,9 +1,11 @@
-import { ipAddress } from '@vercel/functions';
-import { NextRequest, NextResponse } from 'next/server';
+import { ipAddress, geolocation } from "@vercel/functions";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-    console.log("I got to API")
-    const ip = ipAddress(req);
-    console.log("user Ip:", ip);
-    return Response.json({ ip });
+  console.log("I got to API");
+  const ip = ipAddress(req);
+  const details = geolocation(req);
+  console.log("user geolaocation:", details);
+  console.log("user Ip:", ip);
+  return Response.json({ ip });
 }
